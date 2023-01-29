3360c76a206e7254f05cf25c93bb8f5c4a53f251
Vishnu <vishnu20020320@gmail.com>
2023-01-29 17:06:54 +0530

    Message: Delete functionality fixed and optimized by removing the loadNotes() function

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ var getNotesFromMem = async () => {
 }
 
 var loadNotes = () => {
-    if(notes.length == 0) {    // notes == undefined || 
+    if(notes.length == 0) {
         $('#savedNotes').hide();
         $('#noNotesMessage').show();
         return;
@@ -19,7 +19,7 @@ var loadNotes = () => {
     $('#noNotesMessage').hide();
     $('#savedNotes').show();
     $('#savedNotes').empty();
-    var div_notes = document.createElement('div');
+    var div_notes = $('#savedNotes');
     notes.forEach((note) => {
         var div_note = document.createElement('div');
         // div_note.setAttribute('style', 'border:2px solid black; padding: 5px; margin: 5px 2px; overflow-wrap: break-word');
@@ -27,28 +27,23 @@ var loadNotes = () => {
         div_note.setAttribute('class', 'note');
 
         var div_noteMessage = document.createElement('div');
-        div_noteMessage.setAttribute('class', 'note-message');
+        div_noteMessage.setAttribute('class', 'note_message');
         div_noteMessage.innerText = note.message;
         div_note.append(div_noteMessage);
 
         var small_date = document.createElement('small');
-        small_date.innerHTML = '<i>' + note.createdTime + '</i>';
+        small_date.innerHTML = '<em>' + note.createdTime + '</em>';
         div_note.append(small_date);
-        div_note.append(' ');
 
         var btn_delete = document.createElement('button');
-        btn_delete.setAttribute('id', note.id);
-        btn_delete.setAttribute('class', 'delete');
+        btn_delete.setAttribute('class', 'delete_btn');
         btn_delete.innerText = 'Delete';
         btn_delete.click(() => {
-            this.trigger('deleteNote');
+            deleteNote();
         });
         div_note.append(btn_delete);
-
         div_notes.append(div_note);
     });
-    $('#savedNotes').append(div_notes);
-    $('#svaedNotes').on('deleteNote', deleteNote);
 }
 
 var saveNote = () => {
@@ -62,32 +57,61 @@ var saveNote = () => {
     currNote.id = genId();
     currNote.message = noteMessage;
     currNote.createdTime = getCurrentTime();
-    notes.splice(0, 0, currNote);
-    loadNotes();
-    chrome.storage.sync.set({browserNOTES: notes});
+    // notes.push(currNote);    // for inserting at last
+    
+    var div_currNote = document.createElement('div');
+    div_currNote.setAttribute('id', currNote.id);
+    div_currNote.setAttribute('class', 'note');
+    var div_noteMessage = document.createElement('div');
+    div_noteMessage.setAttribute('class', 'note_message');
+    var small_date = document.createElement('small');
+    var btn_delete = document.createElement('button');
+    btn_delete.setAttribute('class', 'delete_btn');
+    btn_delete.innerText = 'Delete';
+    btn_delete.click(() => {
+        deleteNote();
+    });
+    small_date.innerHTML = "<em>" + currNote.createdTime + "</em>";
+    div_noteMessage.innerText = currNote.message;
+    div_currNote.append(div_noteMessage);
+    div_currNote.append(small_date);
+    div_currNote.append(btn_delete);
+    $('#savedNotes').prepend(div_currNote);
     $('#noteMessage').val('');
+    
+    notes.splice(0, 0, currNote);    // for inserting at indx 0
+    if(notes.length > 0) {
+        $('#noNotesMessage').hide();
+        $('#savedNotes').show();
+
+    }
+    chrome.storage.sync.set({browserNOTES: notes});
 }
 
 var deleteNote = (e) => {
+    if(e.target.getAttribute('class') != 'delete_btn') {
+        return;
+    }
     var noteId = e.target.parentNode.getAttribute('id');
-    console.log(noteId);
+    $("#" + noteId).remove();
     var updatedNotes = notes.filter(note => {
         return note.id != noteId;
     });
     notes = updatedNotes;
-    loadNotes();
-    console.log('After deleting a note');
-    console.table(notes);
+    if(notes.length == 0) {
+        $('#savedNotes').hide();
+        $('#noNotesMessage').show();
+    }
     chrome.storage.sync.set({browserNOTES: notes});
 }
 
 var deleteAll = () => {
     if(window.confirm("All the notes will be deleted completely.")) {
+        $('#savedNotes').html('');
+        $('#savedNotes').hide();
+        $('#noNotesMessage').show();
         notes = [];
-        chrome.storage.sync.set({browserNOTES: []}, () => {
-            loadNotes();
-            // window.alert("All the notes are deleted!");
-        });
+        chrome.storage.sync.set({browserNOTES: []});
     }
 }
 
@@ -119,6 +143,6 @@ $(document).ready(async () => {
     loadNotes();
     $('#saveNote').click(saveNote);
     $('#deleteAll').click(deleteAll);
-    $('.delete').click(deleteNote);
-    getCurrentTime();
+    $('#savedNotes').click(deleteNote);
+    // $('.delete').click(deleteNote);
 });
